6f21b716d0fcddfd6d2da736206ecce9c93b8ca2
Nerf-Chamber <lzpobletpobletos@gmail.com>
2026-02-03 15:09:55 +0100

    Door done!

--- Assets/Scripts/SceneObjects/Door.cs
+++ Assets/Scripts/SceneObjects/Door.cs
@@ -3,18 +3,26 @@ using UnityEngine;
 public class Door : MonoBehaviour, IInteractable
 {
     bool hasBeenOpened = false;
+    bool open = false;
 
+    float initialYRotation;
+
+    private void Awake() => initialYRotation = transform.eulerAngles.y;
+    private void Update() 
+    {
+        if (open && transform.eulerAngles.y < initialYRotation + 90) OpenDoor(); 
+    }
     public void Interact(Player player)
     {
         Item playerItem = player.GetCurrentItem();
         if (playerItem is Key key && !hasBeenOpened)
         {
-            if (key.associatedDoor == this) 
-            { 
-                // Here code for rotating door
-                Debug.Log("Rotate door"); 
-                hasBeenOpened = true;
-            }
+            if (key.associatedDoor == this) open = true;
         }
+    }
+    private void OpenDoor()
+    {
+        transform.Rotate(new Vector3(0, Time.deltaTime * 90, 0), Space.World);
+        hasBeenOpened = true;
     }
 }
--- Assets/Scripts/SceneObjects/Player.cs
+++ Assets/Scripts/SceneObjects/Player.cs
@@ -27,7 +27,7 @@ public class Player : MonoBehaviour, InputSystem_Actions.IPlayerActions
     private Vector2 tempDirection = Vector2.zero;
 
     // Jump force fixa
-    private int jumpForce = 30;
+    private int jumpForce = 40;
     private int speed = 0;
 
     private bool isMoving = false;
